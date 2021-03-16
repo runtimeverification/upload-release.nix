1e5814a0e3cf49c3d750184c3ac4c4ef0af5e85f
Thomas Tuegel <ttuegel@mailbox.org>
2021-03-16 15:49:58 -0500

    Remove random string from release.nix filename

--- release.sh
+++ release.sh
@@ -3,9 +3,10 @@
 
 url="https://github.com/$GITHUB_REPOSITORY"
 rev="$GITHUB_SHA"
-trap 'rm -f "$release_json" "$release_nix"' INT EXIT
-release_json="$(mktemp "release-XXXXXXXXXX.json")"
-release_nix="$(mktemp "release-XXXXXXXXXX.nix")"
+trap 'rm -fr "$release_tmp"' INT EXIT
+release_tmp="$(mktemp --tmpdir -d "release-XXXXXXXXXX")"
+release_json="$release_tmp/release.json"
+release_nix="$release_tmp/release.nix"
 nix-prefetch-git --url "$url" --rev "$rev" --fetch-submodules > "$release_json"
 sha256=$(jq -r '.sha256' < "$release_json")
 cat > "$release_nix" <<EOF
